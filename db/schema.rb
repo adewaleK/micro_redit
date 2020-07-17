ec756aefef4ed1ab97e2b3b7f47de77edbad217a
adewaleK <bisdam91@gmail.com>
2020-07-17 02:12:22 +0100

    adds Post model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_002615) do
+ActiveRecord::Schema.define(version: 2020_07_17_005226) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
