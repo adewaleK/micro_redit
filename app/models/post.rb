0a3c29bd0bd07edb75fdab40cfcf6757933016a1
adewaleK <bisdam91@gmail.com>
2020-07-17 03:02:55 +0100

    adds Comment model and associates it with Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates_presence_of :body
 
   belongs_to :user
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_012322) do
+ActiveRecord::Schema.define(version: 2020_07_17_014811) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
