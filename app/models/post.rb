a2383d6f79790ee71f77d6a2ef712420bfa2fc68
adewaleK <bisdam91@gmail.com>
2020-07-17 02:38:44 +0100

    establishes relationship between User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :body
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates_presence_of :username
+  has_many :posts
 end
