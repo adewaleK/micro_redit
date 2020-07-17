74aca2cffc7158ee56774be8ba36c2f5a481f4d4
adewaleK <bisdam91@gmail.com>
2020-07-17 03:50:44 +0100

    establishes relationsghip between Comment and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates_presence_of :username
   has_many :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_014811) do
+ActiveRecord::Schema.define(version: 2020_07_17_021214) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
